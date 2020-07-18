31fbbde4890775f7e4934065f7a8717367d715a0
jakob82 <selfdeterminedhermit@gmail.com>
2020-07-18 13:29:38 +0100

    reviewing-python-code

--- _/PYTHON/poetryHashcat.py
+++ _/PYTHON/poetryHashcat.py
@@ -1,26 +1,27 @@
 from array import *
+import itertools
 
-verse1 = ["Caminhava em silêncio, completamente atónito, num gesto poli-enviesado",
+verses = [] 
+
+verses.append(["Caminhava em silêncio, completamente atónito, num gesto poli-enviesado",
             "Caminhava num frenesim electrico, que meta-dielecticamente me ostracizava",
             "Caminhava por um cardápio de faunos, ali no meio dos cipestres, para que incognitamente se foram",
-            "Morria, incognito, para lentamente me olvidar, por entre um nigredo infra-circunpescrito"];
+            "Morria, incognito, para lentamente me olvidar, por entre um nigredo infra-circunpescrito"]);
 
-verse2 = ["Para que ali, por entre a umbra, e por entre o sopro gélido benzida em mim a esfera do caos",
+verses.append(["Para que ali, por entre a umbra, e por entre o sopro gélido benzida em mim a esfera do caos",
           "Para que por entre um silêncio incógnito, viesse em mim teu maldito cardapio da lei",
           "Para que por entre um gesto subito, que para lá de mim, subitamente se envieza",
-          "Para que por entre o inicio de uma possivel acefaleia pudesse em mim benzer o desterro ultimo da sombra da minha ignorancia"];
+          "Para que por entre o inicio de uma possivel acefaleia pudesse em mim benzer o desterro ultimo da sombra da minha ignorancia"]);
 
-verse3 = ["Viesse em mim silêncio intempério que para lá de mim te revigoras, sombra poli-arcânica do fim dos dias",
+verses.append(["Viesse em mim silêncio intempério que para lá de mim te revigoras, sombra poli-arcânica do fim dos dias",
             "viesse em mim um desterro incognito, que para lá de mim, subitamente se envieza caida em mim a chama da tua flutulência",
             "viesse em mim, um chama arcânica, que subitamente se multiplica e desmultiplica, à sombra do gesto incognito, de um amor que era a lei",
-            "e nisto via a sombra revigorada, de um gesto poli-dissidente, que longe de mim, se aliena incognito para lá do luar"];
+            "e nisto via a sombra revigorada, de um gesto poli-dissidente, que longe de mim, se aliena incognito para lá do luar"]);
 
-verse4 = ["que justa e inefacticamente se permuta por entre o pulsar quantico da esquadria do universo",
-            "que justa e meta-dissidentemente se envieza para lá da sombra das malditas horas",];
+verses.append(["que justa e inefacticamente se permuta por entre o pulsar quantico da esquadria do universo",
+            "que justa e meta-dissidentemente se envieza para lá da sombra das malditas horas"]);
 
 
-for x in verse1:
-    for y in verse2:
-        for z in verse3:
-            for a in verse4:
-                print(x, y, z, a) 
+lines = [line for verse in verses for line in verse]
+for verse in itertools.permutations(lines, 4):
+	print("".join(verse)) 
