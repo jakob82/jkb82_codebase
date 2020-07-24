a7fd654d4c5784f167b371ec99ce35f881c8343e
jakob82 <selfdeterminedhermit@gmail.com>
2020-07-24 11:22:48 +0100

    adding-python-tuts

---
+++ _/CHEAT_SHEETS/PYTHON/tiagoPythonTuts/11.py
@@ -0,0 +1,8 @@
+print("what is your first name my dear?", end=' ')
+_firstName=input();
+print("How old are you sweet heart?", end=' ')
+_myAge=input();
+print("Where were you born my love?", end=' ')
+_cityBorn=input();
+
+print(f"So, your name is {_firstName}, you are {_myAge} years old, and you were born in {_cityBorn}.")
---
+++ _/CHEAT_SHEETS/PYTHON/tiagoPythonTuts/12.py
@@ -0,0 +1,5 @@
+_firstName=input("what is your first name my dear?")
+_myAge=input("How old are you sweet heart?")
+_cityBorn=input("Where were you born my love?")
+
+print(f"So, your name is {_firstName}, you are {_myAge} years old, and you were born in {_cityBorn}.")
